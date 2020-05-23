1ccc4fa1b759f46b351d3f3cde3e419197dd9d78
KarmaZA <EVRJON003@myuct.ac.za>
2020-05-24 00:18:55 +0200

    Enemy Spawn added
    Added a random spawner for a single enemy prefab. can easily be expanded upon

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public float xOffset = 5f;
+    public Camera cam;
+
+    public bool SpawnEnabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0f, 5f);
+    }
+
+    void Spawn()
+    {
+        if (SpawnEnabled)
+        {
+            UnityEngine.Vector2 moSp;
+            moSp.x = cam.transform.position.x + xOffset;
+            moSp.y = Random.Range(-10, 10);
+            GameObject enem = Instantiate(enemy, moSp, UnityEngine.Quaternion.identity);
+            Destroy(enem, 4f);
+        }
+    }
+    
+}
