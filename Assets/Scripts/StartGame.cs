722abfb5e0359610d5bbd9e8ad72757236e58e0e
KarmaZA <EVRJON003@myuct.ac.za>
2020-05-24 23:01:54 +0200

    Added menu Scene and play button
    Basic menu with Play load forest scene

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            button.onClick.AddListener(LoadNextLevel);
+        }
+        catch
+        {
+            Debug.LogError("Button Null Pointer exception");
+        }
+    }
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
